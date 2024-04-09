7d9247d5605cfc302c6a54003d2900edf45253e5
Андрей Королев <ander_kor@mail.ru>
2024-04-09 20:15:54 +0300

    style: сверстать хедер

--- src/pages/main-page/main-page.vue
+++ src/pages/main-page/main-page.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-
+  import { AppHeader } from '@/shared/app-header';
 </script>
 
 <template>
-  <h1>It's main page</h1>
+  <main class="mx-auto rounded-xl shadow-2xl h-screen w-4/5 bg-white my-20">
+    <app-header />
+  </main>
 </template>
---
+++ src/shared/app-header/app-header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="border-b border-b-slate-300 flex justify-between p-10">
+    <div class="flex items-center gap-4">
+      <img
+        src="/logo.png"
+        alt="Логотип магазина"
+        class="w-12"
+      />
+      <div>
+        <h1 class="uppercase font-bold text-xl">Иван да лапти</h1>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+    <ul class="flex items-center gap-4">
+      <li class="flex gap-2 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/cart.svg" />
+        <b>1205 руб.</b>
+      </li>
+      <li class="flex gap-2 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/heart.svg" />
+        <span>Закладки</span>
+      </li>
+      <li class="flex gap-2 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/profile.svg" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
